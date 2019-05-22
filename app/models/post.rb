9bc011dbf11fdf86c8c37e3c7b832670135182b2
MarcusMellorMIM <marcus@mellor.com>
2019-05-22 10:52:53 +0100

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,24 @@
 class Post < ActiveRecord::Base
+  
+    validates :title, presence:true 
+    validates :content, length: { minimum:250 }
+    validates :summary, length: { maximum:250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction), message: "%{value} is not a valid category" }
+    validate  :clickbait
+
+    def clickbait
+        clickbaitword=false
+        if title
+            check=["Won't Believe", "Secret", "Top [0-9]", "Guess"] 
+            check.each do |word|
+                if title.match?word
+                 clickbaitword=true
+                end         
+            end        
+            if !clickbaitword
+             errors.add(:title, "Not click bait")
+            end
+        end
+    end
+
 end
