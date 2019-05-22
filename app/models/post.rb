bae7498c12be755c610a62014d9eb0e9a06a0cf2
Ross Mawdsley <rossmawd@gmail.com>
2019-05-22 11:52:45 +0100

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
-class Post < ActiveRecord::Base
+class MyValidator < ActiveModel::Validator
+  def validate(record)
+    options = ["Won't Believe", 'Secret', 'Top [number]', 'Guess']
+
+    unless !record.title.nil? && record.title.match?(/Won't Believe|Secret|Top [0-100]|Guess/)
+      record.errors[:title] << 'Not Clickbatey!'
+    end
+  end
 end
+
+class Post < ActiveRecord::Base
+  include ActiveModel::Validations
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] } # ,
+  # message: "%{value} is not a valid size" }
+  validates_with MyValidator
+end
+
+# Post category is either Fiction or Non-Fiction This step requires an inclusion validator, which was not outlined in the README lesson. You'll need to refer to the Rails guide to look up how to use it.
