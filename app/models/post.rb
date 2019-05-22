ca5f0752d85e77f26cabbd433945b4f1ad5c9752
Lauren Carne <oreocookie@hotmail.co.uk>
2019-05-22 10:39:30 +0100

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
+require 'pry'
+
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validate :clickbait
+
+  def clickbait
+    unless self.title != nil && self.title.match?(/Won't Believe|Secret|Top 1..100|Guess/)
+      errors.add(:title, "Title is not on the list")
+    end
+  end
 end
